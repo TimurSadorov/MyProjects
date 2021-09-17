3fa49638a6c898201897090cc4fc40d46fb1f76e
Sadorov Timur <sadorov2001@mail.ru>
2021-09-17 21:21:48 +0300

    update tests

--- DelaunayTriangulation/DelaunayTriangulationBySweepingLineMethod/TimeMeter.cs
+++ DelaunayTriangulation/DelaunayTriangulationBySweepingLineMethod/TimeMeter.cs
@@ -12,20 +12,25 @@ namespace DelaunayTriangulationBySweepingLineMethod
     {
         public double MeasureTriangulationExecutionTimeInMs(int repetitionCount, int amountOfPoints)
         {
-            var data = new List<PointF>();
+
+            var data = new PointF[repetitionCount][];
             var r = new Random();
 
-            for (int i = 0; i < amountOfPoints; i++)
-                data.Add(new PointF(r.Next(), r.Next()));
+            for (int j = 0; j < repetitionCount; j++)
+            {
+                data[j] = new PointF[amountOfPoints];
+                for (int i = 0; i < amountOfPoints; i++)
+                    data[j][i] = new PointF(r.Next(), r.Next());
+            }
 
-            DelaunayTriangulator.CalculateDelaunayTriangulation(data);
+            DelaunayTriangulator.CalculateDelaunayTriangulation(data[0]);
             GC.Collect();
             GC.WaitForPendingFinalizers();
 
             var stopWatch = new Stopwatch();
             stopWatch.Start();
             for (int i = 0; i < repetitionCount; i++)
-                DelaunayTriangulator.CalculateDelaunayTriangulation(data);
+                DelaunayTriangulator.CalculateDelaunayTriangulation(data[i]);
             stopWatch.Stop();
 
             return stopWatch.Elapsed.TotalMilliseconds / repetitionCount;
